f4cad6256ab936a2fe0192359baa84c8aeec3910
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-01-25 00:12:34 +1100

    mWatchOS 0.2
    -Added alert template
    -Ironed out kinks with Main loop

--- clock.py
+++ clock.py
@@ -25,12 +25,16 @@ I2C_NUM_COLS = 20
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=40000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
+#Global Variables
+
+State = 0
+
 #Displays on boot
 
 def Boot():
     lcd.clear()
     lcd.move_to(0,0)
-    lcd.putstr("  mWatchOS 0.1  ")
+    lcd.putstr("  mWatchOS 0.2  ")
     lcd.move_to(0,1)
     lcd.putstr("(C) MSG 24/1/22")
     sleep(3)
@@ -43,15 +47,31 @@ def Main():
         for i in range(179):
             lcd.move_to(2,1)
             lcd.putstr(str(TimeParse()))
+            Alerts()
             if TimeParse()[2] == 0:
                 lcd.move_to(0,1)
                 lcd.putstr("                ")
-                sleep(0.9)
         Date()
-        lcd.putstr(str(DateParse()))
-        sleep(3)
+        for i in range(59):
+            lcd.move_to(1,1)
+            lcd.putstr(str(DateParse()))
+            Alerts()
+
+#Alerts are checked once per cycle
+        
+def Alerts():
+    if TimeParse()[0] == 0 and TimeParse()[1] == 0 and TimeParse()[2] <= 1:
+        AlertMessage()
+        lcd.putstr("DefaultAlertMesg")
+        sleep(10)
+        Main()
 
 #Changes the heading
+
+def AlertMessage():
+    lcd.clear()
+    lcd.putstr("   New Alert    ")
+    lcd.move_to(0,1)
 
 def Time():
     lcd.clear()
